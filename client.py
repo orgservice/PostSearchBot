2536d805c090b89e349c482b75eb1366b69a2ca1
orgservice <146081596+orgservice@users.noreply.github.com>
2024-07-21 19:02:39 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,16 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from info import *
 from pyrogram import Client
 
 class Bot(Client):   
     def __init__(self):
         super().__init__(   
-           "vj-post-search-bot",
+           "Post-Search-Bot",
             api_id=API_ID,
             api_hash=API_HASH,           
             bot_token=BOT_TOKEN,
             plugins={"root": "plugins"})
     async def start(self):                        
         await super().start()  
-        print("Bot Started 🔧 Powered By @VJ_Botz")   
+        print("Bot Started 🔧")   
     async def stop(self, *args):
         await super().stop()
