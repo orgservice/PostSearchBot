27b098357c0844b53a64bab750afa26ad2e57d72
orgservice <146081596+orgservice@users.noreply.github.com>
2024-07-21 19:01:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from client import Bot
 
-print("Bot Started 👍 Powered By @VJ_Botz")
+print("Bot Started 👍")
 Bot().run()
 
