2a20acacc200c806372c923a72fc0f3657c8d092
orgservice <146081596+orgservice@users.noreply.github.com>
2024-07-21 19:26:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "12549463"))
+API_HASH     = environ.get("API_HASH", "fddc277f921e644b9b439a61d7f92bee")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "6800494318:AAEsqTZxU8wj_AaTIn1UHIYwfgHmm2o789A")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://uhdprime:uhdprime@cluster0.ry5y4yk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002100505904"))
+ADMIN        = int(environ.get("ADMIN", "1925422909"))
+CHANNEL      = environ.get("CHANNEL", "@ORGPrime")
