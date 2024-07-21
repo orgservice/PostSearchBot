cd633385bb71ef8c49e11fb8ef4ca904c2616cac
orgservice <146081596+orgservice@users.noreply.github.com>
2024-07-21 19:29:24 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from utils import *
 from pyrogram import Client, filters
 from plugins.generate import database 
@@ -18,8 +14,8 @@ async def start(bot, message):
         InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
         InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
     ],[
-        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/ORGPrime"),
+        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/moviespremium")
     ]]
     await message.reply(text=script.START.format(message.from_user.mention),
                         disable_web_page_preview=True,
