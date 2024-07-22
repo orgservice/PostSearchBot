e1bc20af4610a90a616fae4c838c9527037925f0
orgservice <146081596+orgservice@users.noreply.github.com>
2024-07-22 08:41:54 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -53,12 +53,11 @@ async def search(bot, message):
               buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
           msg = await message.reply_photo(photo="https://graph.org/file/e06089d66b2b556816e3d.jpg",
                                           caption="<b><I>🔺 I Couldn't find anything related to Your Query 😕\n\n🔻 Did you mean any of these?</I></b>", 
-                                          reply_markup=InlineKeyboardMarkup(buttons)
-                                         )
+                                          reply_markup=InlineKeyboardMarkup(buttons))
        else:
-           await send_message_in_chunks(bot, message.chat.id, head+results)
-           await asyncio.sleep(30)
-           await msg.delete()
+          await send_message_in_chunks(bot, message.chat.id, head+results)
+          await asyncio.sleep(30)
+          await msg.delete()
            return
     except:
         pass
@@ -79,6 +78,8 @@ async def recheck(bot, update):
        return await update.answer("That's not for you! 👀", show_alert=True)
 
     m=await update.message.edit("**Searching..💥**")
+    await asyncio.sleep(60)
+    await m.delete()
     id      = update.data.split("_")[-1]
     query   = await search_imdb(id)
     channels = (await get_group(update.message.chat.id))["channels"]
@@ -93,12 +94,11 @@ async def recheck(bot, update):
                results += f"<b><I>♻️🍿 {name}</I></b>\n\n🔗 {msg.link}</I></b>\n\n"
        if bool(results)==False:          
           return await update.message.edit("<b>ᴍᴏᴠɪᴇ ɴᴏᴛ ꜰᴏᴜɴᴅ ɪɴ ᴅᴀᴛᴀʙᴀꜱᴇ...\n\nʀᴇᴀsᴏɴ :-\n1) ᴏ.ᴛ.ᴛ ᴏʀ ᴅᴠᴅ ɴᴏᴛ ʀᴇʟᴇᴀsᴇᴅ\n2) ɴᴏᴛ ᴜᴘʟᴏᴀᴅᴇᴅ ʏᴇᴛ\n3) Sᴘᴇʟʟɪɴɢ Mɪꜱᴛᴀᴋᴇ\n\nᴘʟᴇᴀꜱᴇ reqυeѕт тo ɢroυp ᴀᴅᴍɪɴ🔻</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]]))
-       await send_message_in_chunks(bot, update.message.chat.id, head+results)
+           await send_message_in_chunks(bot, update.message.chat.id, head+results)
+           await update.message.delete(60)
     except Exception as e:
        await update.message.edit(f"❌ Error: `{e}`")
-    await update.message.delete(60)
-    await m.delete(60)
-    
+
 @Client.on_callback_query(filters.regex(r"^request"))
 async def request(bot, update):
     clicked = update.from_user.id
