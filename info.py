427249814c27aba96d28fc6d6d299cf686198950
orgservice <146081596+orgservice@users.noreply.github.com>
2024-09-17 20:20:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "12549463"))
-API_HASH     = environ.get("API_HASH", "fddc277f921e644b9b439a61d7f92bee")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "6800494318:AAEsqTZxU8wj_AaTIn1UHIYwfgHmm2o789A")
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://uhdprime:uhdprime@cluster0.ry5y4yk.mongodb.net/?retryWrites=true&w=majority")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002100505904"))
-ADMIN        = int(environ.get("ADMIN", "6630344997"))
+API_ID       = int(environ.get("API_ID", "20778875"))
+API_HASH     = environ.get("API_HASH", "c49ecc77f2abfaedb1bd6bca14559702")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7154196481:AAHNIxoqSvIUOwc9Aq3U0M7DO7JKM15mn54")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://clone:clone@clone.omngv.mongodb.net/?retryWrites=true&w=majority&appName=clone")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002319541032"))
+ADMIN        = int(environ.get("ADMIN", "6924461727"))
 CHANNEL      = environ.get("CHANNEL", "@ORGPrime")
